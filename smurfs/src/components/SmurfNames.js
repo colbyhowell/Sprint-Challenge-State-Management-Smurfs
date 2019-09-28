0d52d1d6d5c0b11c2e44b4a22e42df392061c4d1
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-09-27 22:28:08 -0500

    Got delete working

--- smurfs/src/components/SmurfNames.js
+++ smurfs/src/components/SmurfNames.js
@@ -1,7 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 const SmurfNames = props => {
-  return <div>{props.smurfs.name}</div>;
+  const handleDelete = () => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${[props.smurfs.id]}`)
+      .then(window.alert("Smurf Deleted"))
+      .catch(err => console.log(err));
+    window.location.reload();
+  };
+  return (
+    <div className="smurf-container">
+      <div className="smurf-box">
+        <div className="smurf-name">Name:{props.smurfs.name}</div>
+        <div className="smurf-age">Age:{props.smurfs.age}</div>
+        <div className="smurf-height">Height:{props.smurfs.height}</div>
+        <div className="smurf-delete">
+          <button className="smurf-delete" onClick={handleDelete}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SmurfNames;
